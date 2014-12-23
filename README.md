81c86ff6014f8b97a1d957a2acb0b0bd23642a5c
Francisco De La Cruz <helloquico@gmail.com>
2014-12-22 19:29:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Simple Shopping Cart System
+===========================
+
+This is my code for following along with [Ember.js Simple Shopping Cart System](https://www.youtube.com/playlist?list=PL0nNJgE19Y-gyzjrLV1mz2vJyzmLORWt0).
+
+
---
+++ js/app.js
@@ -0,0 +1,14 @@
+App = Ember.Application.create({
+  // Basic logging, e.g. "Transitioned into 'post'"
+  LOG_TRANSITIONS: true
+});
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
