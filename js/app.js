36708078910fa74d663330d8d587cda8788c921f
Francisco De La Cruz <helloquico@gmail.com>
2014-12-22 22:18:56 -0500

    List products on home page

--- js/app.js
+++ js/app.js
@@ -7,8 +7,31 @@ App.Router.map(function() {
   // put your routes here
 });
 
+App.Product = DS.Model.extend({
+  name: DS.attr(),
+  description: DS.attr(),
+  price: DS.attr()
+});
+
+App.ProductAdapter = DS.FixtureAdapter.extend();
+
+App.Product.FIXTURES = [
+  {
+    id: 1,
+    name: 'Playstation 3',
+    description: 'Last Gen console with the best exclusives',
+    price: 200
+  },
+  {
+    id: 2,
+    name: 'Xbox 360',
+    description: 'Last Gen console with the best addons like Kinnect',
+    price: 180
+  }
+];
+
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return this.store.find('product');
   }
 });
